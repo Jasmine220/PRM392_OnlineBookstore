8a9df6b5352c39974f07cb28d9890cf80debf09b
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-10-30 21:44:06 +0700

    add Validate Email

--- app/src/main/java/com/example/onlinebookstore/Controller/Customer/RegisterActivity2.java
+++ app/src/main/java/com/example/onlinebookstore/Controller/Customer/RegisterActivity2.java
@@ -3,6 +3,7 @@ package com.example.onlinebookstore.Controller.Customer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,44 +45,55 @@ public class RegisterActivity2 extends AppCompatActivity {
 
                 String username = getIntent().getStringExtra("username").toString();
                 String password = getIntent().getStringExtra("password").toString();
-                // Create an instance of the ApiService
-                ApiService apiService = ApiClient.getClient().create(ApiService.class);
-                Account account = new Account(username, password);
-                Customer customer = new Customer(name, email, phone);
-                // Create a RegisterRequest object to send to the API
-                RegisterRequest registerRequest = new RegisterRequest(account,customer);
-                registerRequest.setAccount(account);
-                registerRequest.setCustomer(customer);
 
-                // Make the registration API call
-                Call<LoginResponse> call = apiService.register(registerRequest);
-                call.enqueue(new Callback<LoginResponse>() {
-                    @Override
-                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                        if (response.isSuccessful()) {
-                            // Registration was successful
-                            Intent intent = new Intent(RegisterActivity2.this, LoginActivity.class);
-                            startActivity(intent);
-                            showToast("Registration Successful");
-                            // You can handle success actions here, such as navigating to the main screen.
-                        } else {
-                            // Registration failed
-                            showToast("Registration Failed");
-                            Log.e("RegistrationResponse", "Code: " + response.code());
-                            Log.e("RegistrationResponse", "Message: " + response.message());
-                            Log.e("RegistrationResponse", "Body: " + response.errorBody());
-                            // You can handle failure actions here.
+                if (!isValidate(email)) {
+                    showToast("Please input a valid email address.");
+                } else {
+                    // Create an instance of the ApiService
+                    ApiService apiService = ApiClient.getClient().create(ApiService.class);
+                    Account account = new Account(username, password);
+                    Customer customer = new Customer(name, email, phone);
+                    // Create a RegisterRequest object to send to the API
+                    RegisterRequest registerRequest = new RegisterRequest(account, customer);
+                    registerRequest.setAccount(account);
+                    registerRequest.setCustomer(customer);
+
+                    // Make the registration API call
+                    Call<LoginResponse> call = apiService.register(registerRequest);
+                    call.enqueue(new Callback<LoginResponse>() {
+                        @Override
+                        public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                            if (response.isSuccessful()) {
+                                // Registration was successful
+                                Intent intent = new Intent(RegisterActivity2.this, LoginActivity.class);
+                                startActivity(intent);
+                                showToast("Registration Successful");
+                                // You can handle success actions here, such as navigating to the main screen.
+                            } else {
+                                // Registration failed
+                                showToast("Registration Failed");
+                                Log.e("RegistrationResponse", "Code: " + response.code());
+                                Log.e("RegistrationResponse", "Message: " + response.message());
+                                Log.e("RegistrationResponse", "Body: " + response.errorBody());
+                                // You can handle failure actions here.
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        // Handle network or other errors
-                        showToast("Registration Failed. Please check your network connection.");
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<LoginResponse> call, Throwable t) {
+                            // Handle network or other errors
+                            showToast("Registration Failed. Please check your network connection.");
+                        }
+                    });
+                }
             }
         });
+    }
+    boolean isValidate(String email){
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return true;
+        }
+        return false;
     }
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
