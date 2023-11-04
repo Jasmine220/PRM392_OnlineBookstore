9b4ca94fe7a53423f0aa90dc907de98a039971da
Kasa <103961248+Kawoasa@users.noreply.github.com>
2023-11-04 18:09:47 +0700

    Update HomeActivity.java

--- app/src/main/java/com/example/onlinebookstore/Controller/Customer/HomeActivity.java
+++ app/src/main/java/com/example/onlinebookstore/Controller/Customer/HomeActivity.java
@@ -1,16 +1,18 @@
 package com.example.onlinebookstore.Controller.Customer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.onlinebookstore.R;
 
 public class HomeActivity extends AppCompatActivity {
-
+    RecyclerView rv_bookList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        rv_bookList = findViewById(R.id.rv_bookList);
     }
 }
