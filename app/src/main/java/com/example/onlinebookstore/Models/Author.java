af0988f7fc7f141f0b2c25782ffb1564418826c4
Kasa <103961248+Kawoasa@users.noreply.github.com>
2023-11-04 11:00:58 +0700

    Update Author.java

--- app/src/main/java/com/example/onlinebookstore/Models/Author.java
+++ app/src/main/java/com/example/onlinebookstore/Models/Author.java
@@ -1,7 +1,10 @@
 package com.example.onlinebookstore.Models;
 
+import java.util.Collection;
+
 public class Author {
     private int authorId;
 
     private String authorName;
+    private Collection<Book> books;
 }
