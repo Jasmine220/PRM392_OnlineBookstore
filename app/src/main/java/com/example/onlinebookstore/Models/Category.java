9cdb19e67c7bf2775111a58642b9fceb5e78a2a5
Kasa <103961248+Kawoasa@users.noreply.github.com>
2023-11-04 17:41:31 +0700

    Update Category.java

--- app/src/main/java/com/example/onlinebookstore/Models/Category.java
+++ app/src/main/java/com/example/onlinebookstore/Models/Category.java
@@ -4,4 +4,12 @@ public class Category {
     private int categoryId;
 
     private String categoryName;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
 }
