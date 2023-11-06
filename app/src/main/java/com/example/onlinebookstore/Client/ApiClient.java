cf97bdcc306bc21b4ec90ec1f03326823d4205cb
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-11-06 14:46:01 +0700

    Update ApiClient.java

--- app/src/main/java/com/example/onlinebookstore/Client/ApiClient.java
+++ app/src/main/java/com/example/onlinebookstore/Client/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     // Đường dẫn cơ sở của API của bạn
-    private static final String BASE_URL = "http://192.168.0.3:8080/";
+    private static final String BASE_URL = "http://10.33.33.15:8080/";
 
     private static Retrofit retrofit = null;
 
